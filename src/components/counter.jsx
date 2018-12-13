a1c0936ce62afc566895f4b05afad8ba80beb87a
Arturo Ferreiro Personal <a.ferreirov@hotmail.com>
2018-12-12 20:27:10 -0600

    conter class

---
+++ src/components/counter.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
+  };
+
+  //   constructor() {
+  //     super();
+  //     this.handleIncrement = this.handleIncrement.bind(this);
+  //   }
+
+  render() {
+    return (
+      <React.Fragment>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement({ id: 1 })}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        {this.state.tags.length === 0 && "Porfavor crea un nuevo tag"}
+        {this.renderTags()}
+      </React.Fragment>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+
+  handleIncrement = product => {
+    console.log("Increment Clicked", this);
+    console.log("Product", product);
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>¡No hay tags!</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default Counter;
